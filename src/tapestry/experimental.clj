97ea3dd75dc59919067d59d8e7106ebc3d60b602
Ryan Schmukler <rschmukler@gmail.com>
2026-03-19 16:20:57 -0400

    feat: upgrade to JDK 25+ StructuredTaskScope API
    Update experimental StructuredTaskScope related functionality to JDK 25

--- src/tapestry/experimental.clj
+++ src/tapestry/experimental.clj
@@ -1,14 +1,12 @@
 (ns tapestry.experimental
-  (:import [java.util.concurrent StructuredTaskScope$ShutdownOnSuccess]))
-
+  (:import [java.util.concurrent StructuredTaskScope StructuredTaskScope$Joiner]))
 
 (defmacro alts
   "Execute the given `exprs` in parallel and return the result of the first one to return,
   shutting down all others with an interrupted exception."
   [& exprs]
   (let [scope (gensym "scope")]
-    `(with-open [~scope (StructuredTaskScope$ShutdownOnSuccess.)]
+    `(with-open [~scope (StructuredTaskScope/open (StructuredTaskScope$Joiner/anySuccessfulResultOrThrow))]
        ~@(for [expr exprs]
-           `(.fork ~scope (fn [] ~expr)))
-       (.join ~scope)
-       (.result ~scope #(throw %)))))
+           `(.fork ~scope ^java.util.concurrent.Callable (fn [] ~expr)))
+       (.join ~scope))))
